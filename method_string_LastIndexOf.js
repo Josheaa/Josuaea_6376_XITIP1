5e49a6c732b555c5bcbe0ec616ad8187e4069b1e
Josheaa <josua99579@gmail.com>
2023-08-08 10:06:39 +0700

    Ini hasil copypaste dari gcr

---
+++ method_string_LastIndexOf.js
@@ -0,0 +1,19 @@
+const readline = require(`readline`);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Last Index Of
+rl.question("Masukkan kalimat pertama: ", (kalimat) => {
+    rl.question("Masukkan kata yang ingin Anda cari: ", (kataCari) => {
+        // Gunakkan method LastindexOf untuk mencari kataCari dalam kalimat
+        const indeksKataTerakhir = kalimat.indexOf(kataCari);
+        if (indeksKataTerakhir !== -1) {
+            console.log(`Kata '${kataCari}' ditemukkan pada indeks Terakhir: ${indeksKataTerakhir}`);
+        } else {
+            console.log(`Kata '${kataCari}' tidak ditemukkan dalam kalimat.`)
+        }
+        rl.close();
+    });
+});
